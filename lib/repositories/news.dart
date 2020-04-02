da41f6270397ac070f7f6ec97cc1ed1e5cfc0384
marcelopr <gh.marcelo@hotmail.com>
2020-04-02 15:36:13 -0300

    Create Repositories folder

--- lib/repositories/news.dart
+++ lib/repositories/news.dart
@@ -4,10 +4,11 @@ import 'package:newsapp/models/article_model.dart';
 import 'package:http/http.dart' as http;
 
 class News {
-  Future<List<Article>> getNews(String category, String keyWords) async {
+  Future<List<Article>> getNews(
+      String category, int page, String keyWords) async {
     List<Article> news = [];
     String url = keyWords == null
-        ? 'https://newsapi.org/v2/top-headlines?pageSize=20&category=$category&country=br&apiKey=$newsApiKey'
+        ? 'https://newsapi.org/v2/top-headlines?pageSize=15&page=$page&category=$category&country=br&apiKey=$newsApiKey'
         : 'https://newsapi.org/v2/top-headlines?q=$keyWords&pageSize=20&category=$category&country=br&apiKey=$newsApiKey';
 
     var response = await http.get(url);
